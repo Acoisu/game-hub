601bde1364cf8ac494512583e83e9c39c8ecee2b
Acoisu <aym.delafaire@gmail.com>
2023-07-10 15:18:02 +0200

    Adding platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { Game } from "../hooks/useGames";
 import {
   Card,
   CardBody,
+  CardFooter,
   GridItem,
   Heading,
   Hide,
   Image,
+  Text,
 } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -15,10 +17,18 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card border="2px" borderColor="grey" borderRadius={10} overflow="Hidden">
+    <Card
+      border="2px"
+      borderColor="gray.500"
+      borderRadius={10}
+      overflow="Hidden"
+    >
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGameResponse {
@@ -27,6 +34,7 @@ const useGames = () => {
         if (error instanceof CanceledError) return;
         setError(error.message);
       });
+    console.log({ gameList });
     return () => controller.abort();
   }, []);
   return { gameList, error };
