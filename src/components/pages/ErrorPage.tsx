40e5a1b71b645c2e52c439f667cd2210a4f90db2
Acoisu <aym.delafaire@gmail.com>
2023-08-03 19:14:39 +0200

    handling errors

---
+++ src/components/pages/ErrorPage.tsx
@@ -0,0 +1,22 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import Navbar from "../Navbar";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  isRouteErrorResponse(error);
+
+  return (
+    <>
+      <Navbar />
+      <Box padding={5}>
+        <Heading>Oops...</Heading>
+        <Text>
+          {isRouteErrorResponse(error) ? "Inavalid page" : "Unexpected error"}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
