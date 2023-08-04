b7da509807735e7085a13842ae57676d7432db61
Acoisu <aym.delafaire@gmail.com>
2023-08-04 16:10:38 +0200

    refactoring- entity interface

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -2,20 +2,9 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
 import useGameQueryStore, { GameQuery } from "../store/store";
 import APIClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "./usePlatforms";
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
-
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  genre: string;
-  description_raw: string;
-}
 
 const useGames = () => {
   const gameQuery = useGameQueryStore((s) => s.gameQuery);
--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -1,15 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APIClient from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-  slug: string;
-}
 
 const useGenres = () =>
   useQuery({
